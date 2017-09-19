309c67790d683cf2cb8d22f470d899a1f2e46a54
j1u2l3i4a5n <b04207001@ntu.edu.tw>
2017-09-19 19:29:03 +0800

    operate 5 steps and write a file to save the result

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -7,7 +7,27 @@ Created on Tue Sep 19 18:47:14 2017
 
 inputFile=open("filename.txt",'r')
 data=inputFile.read()
+inputFile.close()
 dataList=data.split(' ')
+dataStr=""
+temp=[]
+
 for i in range(len(dataList)):
     dataList[i]=int(dataList[i])
+    
+for i in range(len(dataList)):
+    dataList[i]=dataList[i]**2
 
+for i in  [17, 23, 46, 78, 91]:
+    dataList.append(i)
+
+for i in range(len(dataList)): 
+    temp.append(dataList[len(dataList)-i-1])
+dataList=temp
+    
+for i in dataList:
+    dataStr+=str(i)+" "
+
+outputFile=open("output.txt","w")
+outputFile.write(dataStr)
+outputFile.close()
