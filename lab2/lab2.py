39cad8d69454fe1699469e7cb132886cecf33d22
j1u2l3i4a5n <b04207001@ntu.edu.tw>
2017-09-22 10:39:51 +0800

    use lambda and map to finished the 5 operations

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -8,26 +8,9 @@ Created on Tue Sep 19 18:47:14 2017
 inputFile=open("filename.txt",'r')
 data=inputFile.read()
 inputFile.close()
-dataList=data.split(' ')
-dataStr=""
-temp=[]
 
-for i in range(len(dataList)):
-    dataList[i]=int(dataList[i])
-    
-for i in range(len(dataList)):
-    dataList[i]=dataList[i]**2
-
-for i in  [17, 23, 46, 78, 91]:
-    dataList.append(i)
-
-for i in range(len(dataList)): 
-    temp.append(dataList[len(dataList)-i-1])
-dataList=temp
-    
-for i in dataList:
-    dataStr+=str(i)+" "
+output=' '.join(map(str,(map(lambda x:x**2,map(int,data.split(' ')))+[17, 23, 46, 78, 91])[::-1]))
 
 outputFile=open("output.txt","w")
-outputFile.write(dataStr)
+outputFile.write(output)
 outputFile.close()
